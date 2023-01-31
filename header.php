7de6589a5966fd1901f7e653e8dd98dd79f47881
zayed034 <shabuj182@gmail.com>
2023-01-31 16:45:47 +0600

    modifying header

--- header.php
+++ header.php
@@ -28,8 +28,9 @@ include("config.php");
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">
+    <!-- Font Awesome 
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- ico-font -->
     <link rel="stylesheet" type="text/css" href="assets/css/icofont.css">
